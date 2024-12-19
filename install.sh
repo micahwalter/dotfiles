04f788d521c855047ba0ac397b5bf9ccdba2c918
Micah Walter <47419+micahwalter@users.noreply.github.com>
2024-12-19 12:21:48 -0500

    adding sam cli tool

--- install.sh
+++ install.sh
@@ -9,10 +9,15 @@ sudo apt install neovim -y
 # install tmux
 sudo apt install tmux -y
 
-# install aws tools
+# install aws cli tool
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+
+# install aws sam cli tool
+curl "https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip" -o "aws-sam.zip"
+unzip aws-sam.zip -d aws-sam
+sudo ./aws-sam/install
 
 # install go tools
 echo 'deb [trusted=yes] https://repo.goreleaser.com/apt/ /' | sudo tee /etc/apt/sources.list.d/goreleaser.list
