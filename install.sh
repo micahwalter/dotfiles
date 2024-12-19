a369e2090953978bb3f6a9621e99de2e0f539478
Micah Walter <47419+micahwalter@users.noreply.github.com>
2024-12-19 10:13:47 -0500

    adding basic tooling

--- install.sh
+++ install.sh
@@ -3,4 +3,13 @@
 sudo apt update
 sudo apt upgrade -y
 
+# install neovim
 sudo apt install neovim -y
+
+# install aws tools
+sudo apt install awscli -y
+
+# install go tools
+echo 'deb [trusted=yes] https://repo.goreleaser.com/apt/ /' | sudo tee /etc/apt/sources.list.d/goreleaser.list
+sudo apt update
+sudo apt install goreleaser -y
